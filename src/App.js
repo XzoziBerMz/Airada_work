86ecc4734073ae65a9a7cc6fe9864b7de8492c50
XzoziBerMz <xzoziber.mz@gmail.com>
2024-03-03 16:15:15 +0700

    work

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App_Non() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h1 className="t-green">Resume</h1>
+        </div>
+        <div>
+          <h2>นางสาว ไอรดา นามสกุล: สำราญ</h2>
+        </div>
+        <div>
+          <p>สีที่ชอบ : สีชมพู ฟ้า ม่วง</p>
+        </div>
+        <div>
+          <p>ความสูง : 165 เซน</p>
+        </div>
+        <div>
+          <p>น้ำหนัก : 80 กก.</p>
+        </div>
+        <div>
+          <p>คำคมที่ชอบ : ทำวันนี้ให้ดีที่สุด</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App_Non;
